6597c57bc763122b5423bfd31594a25514d4f78b
andyshafer <andyshafer0715@gmail.com>
2020-12-24 15:26:01 -0500

    2020 Day 15

---
+++ 2020/D15/P2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	vector<int> nums = { 6, 3, 15, 13, 1, 0 };
+	map<int,int> lastSpoken;
+	for(int i = 0; i < nums.size(); i++) {
+		lastSpoken[nums[i]] = i;
+	}
+	for(int i = nums.size(); i < 30000000; i++) {
+		if(lastSpoken.find(nums[i-1]) == lastSpoken.end()) {
+			nums.push_back(0);
+		} else {
+			nums.push_back(i-1-lastSpoken[nums[i-1]]);
+		}
+		lastSpoken[nums[i-1]] = i-1;
+	}
+	cout << nums[29999999] << endl;
+}
